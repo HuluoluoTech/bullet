f5a7615d86a0146ec6836f464846858baafe9153
zTgx <49638837+zTgx@users.noreply.github.com>
2022-08-16 01:02:29 +0800

    Create README.md dddddX This is the commit message #2:
    Refactor cmake commands to lowercases
    
    Add processInput function
    
    Draw triangle
    
    Add GLM
    
    Add freetype
    
    Add Boost

---
+++ README.md
@@ -0,0 +1,15 @@
+# bullet
+bullet engine system application
+
+## Setup ENV
+1/ download glad.zip. Opengl 3.3 - Core   
+2/ `sudo apt install libglfw3-dev`  
+
+## How to run
+```shell
+mkdir my_build & cd my_build
+cmake ..
+./bullet
+```
+there you go...
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,35 @@
-// 必须放在最前面，否则提示:
-// error: #error OpenGL header already included, remove this include, glad already provides it
-//
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
 #include <iostream>
+#include <boost/asio.hpp>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+using namespace boost;
 
 int main(void)
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    std::cout << "Hello world." << std::endl;
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window); 
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+    // Step 1. Assume that the client application has already
+    // obtained the IP-address and the protocol port number.
+    std::string raw_ip_address = "127.0.0.1";
+    unsigned short port_num = 3333;
+    // Used to store information about error that happens
+    // while parsing the raw IP-address.
+    boost::system::error_code ec;
+    // Step 2. Using IP protocol version independent address
+    // representation.
+    asio::ip::address ip_address = asio::ip::address::from_string(raw_ip_address, ec);
+    std::cout << "ip address: " << ip_address.to_string() << std::endl;
+    
+    if (ec.value() != 0) {
+        // Provided IP address is invalid. Breaking execution.
+        std::cout
+        << "Failed to parse the IP address. Error code = "
+        << ec.value() << ". Message: " << ec.message();
+        return ec.value();
     }
-
-    // ower left corner of the window
-    glViewport(0, 0, 800, 600);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-
+    // Step 3.
+    asio::ip::tcp::endpoint ep(ip_address, port_num);
+    // Step 4. The endpoint is ready and can be used to specify a
+    // particular server in the network the client wants to
+    // communicate with.
     return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
 }
